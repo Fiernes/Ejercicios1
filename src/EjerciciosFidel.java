7d5bfe95700f5a5c6c9464ad0e25a7e5e78fc317
Fiernes <fidel255.gu@gmail.com>
2024-06-20 19:43:18 -0600

    cualquier cosa 2

--- src/EjerciciosFidel.java
+++ src/EjerciciosFidel.java
@@ -27,39 +27,5 @@ public class EjerciciosFidel {
         String mensaje = (num1 > num2) ? num1 + " es más grande" : (num1 < num2) ? num2 + " es más grande" : "Estos números son iguales";
 
         System.out.println(mensaje);
-
-        entrada.close();
-    }
-
-    public void Extremos(){
-
-        int numero, mayor = Integer.MIN_VALUE, menor = Integer.MAX_VALUE;
-        boolean continuar = false;
-
-        // Ciclo para leer 5 números enteros
-        for (int i = 1; i <= 5; i++) {
-
-            do{
-                System.out.print("Ingrese el número " + i + ": ");
-                if (!entrada.hasNextInt()){
-                    continuar = true;
-                    System.out.println("Error: Debe ingresar un número entero.");
-                    entrada.next(); // Limpiar entrada
-                }else{
-                    continuar = false;
-
-                }
-            } while(continuar);
-
-            numero = entrada.nextInt();
-
-            // Actualizar valores máximo y mínimo
-            mayor = Math.max(mayor, numero);
-            menor = Math.min(menor, numero);
-        }
-
-        // Imprimir resultados
-        System.out.println("El número mayor es: " + mayor);
-        System.out.println("El número menor es: " + menor);
     }
 }
--- src/main.java
+++ src/main.java
@@ -3,5 +3,7 @@ public class main {
     public static void main(String[] args) {
 
         EjerciciosFidel Fidel = new EjerciciosFidel();
+
+        Fidel.NumeroMayor();
     }
 }
