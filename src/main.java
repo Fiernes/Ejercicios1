65c5eaf0fc3103b3033da0f321811d15b2e9be53
Abner Valiente <mauriciovaliente6@gmail.com>
2024-06-26 20:57:03 -0600

    Subiendo mis ejercicios

--- src/main.java
+++ src/main.java
@@ -5,19 +5,27 @@ public class main {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
-        EjerciciosFidel Fidel = new EjerciciosFidel();
-        //Fidel.encontrarMayor(entrada);
-        //Fidel.encontrarExtremos(entrada);
-        //Fidel.mostrarGraficosAsteriscos(entrada);
-        //Fidel.imprimirCuadradoHueco(entrada);
-        //Fidel.calcularPromedio();
-        //Fidel.imprimirTablaConversiones();
-        //Fidel.imprimirTablaMultiplicacion(entrada);
-        //Fidel.divideYSuma(entrada);
-        //Fidel.HaceFrio(entrada);
-        //Fidel.Alineado(entrada);
-        //Fidel.Estacionamiento(entrada);
-        //Fidel.TorreHanoi(entrada);
-        //Fidel.Potenciando(entrada);
+         EjerciciosAbner abner  = new EjerciciosAbner();
+        //abner.jugandocon3numeros(entrada);
+        //abner.ParImpar(entrada);
+        //abner.gastosquenotengo(entrada);
+        //abner.triangulorecto(entrada);
+        //abner.impar(entrada);
+        //abner.diadepago(entrada);
+        //abner.menuopciones(entrada);
+        //abner.criptofuncion(entrada);
+        //abner.dados(entrada);
+        //abner.ejercicio61(entrada);
+        //abner.lanzamientomoneda(entrada);
+        //abner.pancomido(entrada);
+        //abner.ejercicio91(entrada);
+        //abner.matriz(entrada);
+        //abner.lanzamientodado(entrada);
+        //abner.ejercicio109(entrada);
+        //abner.hospital(entrada);
+       //abner.registro(entrada);
+
+
+
     }
 }
