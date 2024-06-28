e3705bc191b924ef1868d22a6b45984fb40768ab
cesfi105 <giselle1983morales@gmail.com>
2024-06-28 14:25:05 -0600

    con el numero de ejercicio

--- src/main.java
+++ src/main.java
@@ -5,26 +5,46 @@ public class main {
         EjercicioCesar cesar = new EjercicioCesar();
         ClinicaNutricion Fiallos = new ClinicaNutricion();
 
+        //4
         cesar.OperacionesBasicas();
+        //10
         cesar.Cuadrado();
+        //16
         cesar.SepararDigito();
+        //22
         cesar.NumeroMayorMenu();
+        //28
         cesar.Cifrado();
         cesar.Descifrado();
+        //34
         cesar.VentaProductos();
+        //40
         cesar.SeamosPositivos();
+        //46
         cesar.ValorAbsoluto();
+        //52
         cesar.TrazaTuDestino();
+        //58
         cesar.multiplo();
+        //64
         cesar.inverso();
+        //70
         cesar.InstruccionAsistida();
+        //76
         cesar.Alineaésta();
+        //82
         cesar.coregir();
+        //88
         cesar.Trigonometria();
+        //94
         cesar.Vectores();
+        //100
         cesar.burbuja();
+        //106
         cesar.pollos();
+        //12
         cesar.metereologico();
+        //118
         Fiallos.clinica();
 
 }
