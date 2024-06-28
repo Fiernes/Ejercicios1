4737a301e50c9cf7a31b5c920c0e069dd1168cdf
Wasani26 <adrianmartinezbonilla200@gmail.com>
2024-06-28 15:37:28 -0600

    Cambio

--- src/main.java
+++ src/main.java
@@ -3,5 +3,28 @@ public class main {
     public static void main(String[] args) {
 
         EjerciciosAdrian Adrian = new EjerciciosAdrian();
+        //1 //Adrian.EjemploSaludo();
+        //2 //Adrian.MostrandoSello();
+        //8 //Adrian.OrdenarNumeros();
+        //14 //Adrian.TableroDamas();
+        //20 //Adrian.CalculadoraSueldoVendedor();
+        //26 //Adrian.CopiarPegar();
+        //32 //Adrian.Factorial();
+        //38 //Adrian.Rombo();
+        //44 //Adrian.MenuProgram();
+        //50 //Adrian.FiguraV2();
+        //56 //Adrian.AlineaNombre();
+        //62 //Adrian.DisminuyeNum();
+        //68 //Adrian.MultiplicacionPrimaria();
+        //74 // Adrian.Elevation();
+        //80 //Adrian.MiLibreria();
+        //86 //Adrian.ImpArchivo();
+        //92 //Adrian.ejercicio91();
+        //98 //Adrian.MatrizV2();
+        //104 //Adrian.MatrizSentido();
+        //110 //Adrian.PhdMatriz();
+
+
+
     }
 }
