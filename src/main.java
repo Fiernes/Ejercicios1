3de9e22bed6d8da4229aeaa4229a54baa47512c4
Fiernes <fidel255.gu@gmail.com>
2024-06-28 14:30:06 -0600

    Ejercicios de Cesar con numero de ejercicio en el main

--- src/main.java
+++ src/main.java
@@ -7,6 +7,8 @@ public class main {
         Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
         EjerciciosFidel Fidel = new EjerciciosFidel();
         EjerciciosAdrian Adrian = new EjerciciosAdrian();
+        EjercicioCesar cesar = new EjercicioCesar();
+        ClinicaNutricion fiallos = new ClinicaNutricion();
         HospitalManagement h = new HospitalManagement();
         h.hospital();
         //5
@@ -49,6 +51,47 @@ public class main {
         //Fidel.Grafos(entrada);
         //119
         //Fidel.Clinica2();
+        //4
+        cesar.OperacionesBasicas();
+        //10
+        cesar.Cuadrado();
+        //16
+        cesar.SepararDigito();
+        //22
+        cesar.NumeroMayorMenu();
+        //28
+        cesar.Cifrado();
+        cesar.Descifrado();
+        //34
+        cesar.VentaProductos();
+        //40
+        cesar.SeamosPositivos();
+        //46
+        cesar.ValorAbsoluto();
+        //52
+        cesar.TrazaTuDestino();
+        //58
+        cesar.multiplo();
+        //64
+        cesar.inverso();
+        //70
+        cesar.InstruccionAsistida();
+        //76
+        cesar.Alineaésta();
+        //82
+        cesar.coregir();
+        //88
+        cesar.Trigonometria();
+        //94
+        cesar.Vectores();
+        //100
+        cesar.burbuja();
+        //106
+        cesar.pollos();
+        //12
+        cesar.metereologico();
+        //118
+        fiallos.clinica();
 
 
     }
