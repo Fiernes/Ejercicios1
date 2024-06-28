c07c41d8493096b72ae2cd7c8f289736560c3cb8
BayronAg <alexanderfunez874@gmail.com>
2024-06-28 16:37:06 -0600

    ejercicio

---
+++ src/Ejercicios_BayronAg/src/Main.java
@@ -0,0 +1,24 @@
+public class main {
+
+    public static void main (string[] args ) {
+        Bayron Bayron = new Bayron ();
+        Bayron.Operaciones
+        //1   // Ejercicio 3//bayron.OperacionesBasicas
+        //32  // Ejercicio 9//bayron.círculo
+        //63  // Ejercicio 15//bayron.JPanel
+        //104 // Ejercicio 21//bayron.NominaSimple
+        //143  // Ejercicio 27//bayron.SumaSecuenciaEnteros
+        //182  // Ejercicio 33//bayron.PatronesAsteriscos
+        //259  // Ejercicio 39//bayron.RomboAsteriscos
+        //318  // Ejercicio 45//bayron.ExponenciacionSinMathPow
+        //355  // Ejercicio 51//bayron.CalculadoraGeometricaFigura
+        //590  // Ejercicio 57 //bayron.HypotenuseCalculator
+        //628  // Ejercicio 63//bayron.NumerosPerfectos
+        //664  // Ejercicio 69//bayron.RandomArithmetic
+        //773  // Ejercicio 75//bayron.TrigonometricTable
+        //825 // Ejercicio 81//bayron.MyLibrary
+        //889  // Ejercicio 87//bayron.ArchivoMensajes
+        //964  // Ejercicio 99//bayron.PagosVendedores
+        //1000   // Ejercicio 105//bayron.Matrices
+        //1088 // Ejercicio 111//bayron.OperacionesMatrices
+        //1201  // Ejercicio 117//bayron.Persona
