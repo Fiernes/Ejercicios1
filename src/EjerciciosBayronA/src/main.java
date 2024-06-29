be901df6fcde62fc4495e78bc44a40cc1436d62c
BayronAg <alexanderfunez874@gmail.com>
2024-06-28 20:46:47 -0600

    Add files via upload
    Revision

---
+++ src/EjerciciosBayronA/src/main.java
@@ -0,0 +1,50 @@
+public class main {
+
+    Ejerciciosbayron Bayron = new Ejerciciosbayron();
+
+    public void setBayron(Ejerciciosbayron bayron) {
+        Bayron = bayron;
+        // 3
+        bayron.OperacionesBasicas();
+        // 9
+        bayron.Circulo();
+        // 15
+        bayron.Figuras();
+        // 21
+        bayron.NominaSimple();
+
+        // 27
+        bayron.SumaSecuenciaEnteros();
+
+        //33
+        bayron.PatronesAsteriscos();
+
+        // 39
+        bayron.
+
+        // 45
+
+        // 51
+
+        // 57
+
+        // 63
+
+        // 69
+
+        // 75
+
+
+        // 87
+
+        //99
+
+        //105
+
+        // 111
+
+        // 117
+
+
+    }
+}
